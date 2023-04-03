808a9b74b9db484195faf1abbcd8aaf09bf8618d
shrekle <nanonline760@gmail.com>
2023-04-03 07:11:16 -0800

    Initial Commit

---
+++ roastBeefyBeefs/roastBeefyBeefsApp.swift
@@ -0,0 +1,17 @@
+//
+//  roastBeefyBeefsApp.swift
+//  roastBeefyBeefs
+//
+//  Created by adrian garcia on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct roastBeefyBeefsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
